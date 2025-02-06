d1e870718e8f7774b0378b26c6e9f80d64ddf6e5
lizzwizz03 <elizabeth.collins2@bsu.edu>
2025-02-06 12:17:32 -0500

    All tesst ran successfully. Created a wiki user class

---
+++ src/main/java/edu/bsu/cs/wikipedia/wikiUser.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs.wikipedia;
+
+import java.util.Scanner;
+
+public class wikiUser {
+
+    public void findWikiUser(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input a User: ");
+        String wikiUserInput = scan.nextLine();
+    }
+
+
+}
