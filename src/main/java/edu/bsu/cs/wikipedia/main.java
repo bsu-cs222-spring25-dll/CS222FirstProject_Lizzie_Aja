e1675e5c926c34eb333d76bbca15d738f068bc9a
lizzwizz03 <elizabeth.collins2@bsu.edu>
2025-03-05 14:04:22 -0500

    Made corrections to the build.gradle.kts to allow code to properly run. Move all newly added exceptions into the correct folder.

--- src/main/java/edu/bsu/cs/wikipedia/main.java
+++ src/main/java/edu/bsu/cs/wikipedia/main.java
@@ -1,10 +1,9 @@
 package edu.bsu.cs.wikipedia;
 
-import java.io.IOException;
-
 public class main {
     public static void main(String[] args) {
         wikiMenu menu = new wikiMenu();
         menu.runMenu();
+        //comment is here so I could push the project
     }
 }
